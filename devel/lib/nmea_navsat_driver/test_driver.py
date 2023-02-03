54e75b8674369003ed78a717c4836426529d319e
Plantmap3D <aaron.mathew0217@gmail.com>
2023-02-03 13:53:56 -0500

    Removing build and devel folders

--- devel/lib/nmea_navsat_driver/test_driver.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-exec /home/plantmap3d/Desktop/ROS_dev_ws2/PlantMap3D-ROS/devel/share/nmea_navsat_driver/venv/bin/python /home/plantmap3d/Desktop/ROS_dev_ws2/PlantMap3D-ROS/src/gps_module_driver/test/test_driver.py "$@"
