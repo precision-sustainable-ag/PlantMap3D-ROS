c5c31b5c9be6b4130ea1f33e852308c01dea8734
Daniel Ginn <daniel.ginn@live.com.au>
2023-02-07 16:56:21 -0600

    Biomass estimation module has basic species summation

--- src/biomass_estimator_driver/scripts/biomass_estimator.py
+++ src/biomass_estimator_driver/scripts/biomass_estimator.py
@@ -6,11 +6,19 @@ class BiomassEstimator:
     def __init__(self, semantic_array: np.array, heights_array: np.array):
         self.semantic_array = semantic_array
         self.heights_array = heights_array
-        self.biomass_estimates = {'AMAPA': 0.0, 'AMATU': 0.0, 'ECHCG': 0.0, 'PROLO': 0.0, 'ZEAMX': 0.0, 'GLXMA': 0.0}
+        self.biomass_estimates = {'AMAPA': 0, 'AMATU': 0, 'ECHCG': 0, 'PROLO': 0, 'ZEAMX': 0, 'GLXMA': 0}
 
     def run(self) -> Dict:
-        self.biomass_estimates['AMATU'] = 123.45
-        self.biomass_estimates['ECHCG'] = 234.56
+
+        for i in range(np.shape(self.semantic_array)[0]):
+            for j in range(np.shape(self.semantic_array)[1]):
+                if self.semantic_array[i][j] == 0:
+                    self.biomass_estimates['AMAPA'] += 1
+                elif self.semantic_array[i][j] == 1:
+                    self.biomass_estimates['AMATU'] += 1
+                elif self.semantic_array[i][j] == 2:
+                    self.biomass_estimates['ECHCG'] += 1
+
         return self.biomass_estimates
 
 
--- src/biomass_estimator_driver/scripts/tests/test_biomass_estimator.py
+++ src/biomass_estimator_driver/scripts/tests/test_biomass_estimator.py
@@ -4,9 +4,19 @@ from biomass_estimator import BiomassEstimator
 
 class TestBiomassEstimator(TestCase):
 
-    def test_run(self):
-        test_input_1 = np.ones([2, 3])
-        test_input_2 = np.ones([2, 3])
-        bio_est = BiomassEstimator(semantic_array=test_input_1, heights_array=test_input_2)
+    def setUp(self):
+        self.test_input_1 = np.ones([2, 3])
+        self.test_input_2 = np.ones([2, 3])
+
+    def test_should_output_a_dict(self):
+        bio_est = BiomassEstimator(semantic_array=self.test_input_1, heights_array=self.test_input_2)
         biomass_results = bio_est.run()
-        self.assertEqual(biomass_results['AMATU'], 123.45)
+        self.assertEqual(type(biomass_results), dict)
+
+    def test_should_sum_to_five_AMATU(self):
+        self.test_input_1 = np.ones([2, 3])
+        self.test_input_1[0][0] = 0
+        bio_est = BiomassEstimator(semantic_array=self.test_input_1, heights_array=self.test_input_2)
+        biomass_results = bio_est.run()
+        self.assertEqual(biomass_results['AMATU'], 5)
+
