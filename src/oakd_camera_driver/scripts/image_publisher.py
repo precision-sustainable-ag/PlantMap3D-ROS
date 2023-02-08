818466fcd3242af1519b1b24dd0215e3b9d071e7
Plantmap3D <aaron.mathew0217@gmail.com>
2023-02-08 15:21:37 -0500

    Creating a custom ROS message to publish list of ndarrays

--- src/oakd_camera_driver/scripts/image_publisher.py
+++ src/oakd_camera_driver/scripts/image_publisher.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import cv2
-import os
-import rospy 
-from std_msgs.msg import Image
+import rospy
+from rospy.numpy_msg import numpy_msg
+from std_msgs.msg import Int32
+import numpy as np
 
 
-
-def get_camera_data(rgb_image:np.ndarray,depth_map:np.ndarray,segmentation_labels:np.ndarray):
-
-
-    return rgb_image, depth_map, segmentation_labels
+# def get_camera_data(rgb_image:np.ndarray,depth_map:np.ndarray,segmentation_labels:np.ndarray):
 
 
-if __name__ == '__main__':
+#     return rgb_image, depth_map, segmentation_labels
 
-    rgb_image = cv2.imread("~/Desktop/ROS_dev_ws/PlantMap3D-ROS/src/oakd_camera_driver/scripts/mona_lisa.png")
-    depth_map = np.random.randint(0,30,(128,128,3),dtype=int)
-    segmentation_labels = np.random.randint(0,4,(128,128,3),dtype=int)
+
+# if __name__ == '__main__':
+
+#     rgb_image = cv2.imread("~/Desktop/ROS_dev_ws/PlantMap3D-ROS/src/oakd_camera_driver/scripts/mona_lisa.png")
+#     depth_map = np.random.randint(0,30,(128,128,3),dtype=int)
+#     segmentation_labels = np.random.randint(0,4,(128,128,3),dtype=int)
     
 
 
