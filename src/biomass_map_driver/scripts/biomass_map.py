f6cbf432359066b95ddb8bd0861ae0884174d1eb
Daniel Ginn <daniel.ginn@live.com.au>
2023-02-10 14:08:56 -0600

    Update biomass map module to receive numpy arrays

--- src/biomass_map_driver/scripts/biomass_map.py
+++ src/biomass_map_driver/scripts/biomass_map.py
@@ -1,10 +1,10 @@
-from typing import Dict, Tuple
+from typing import Tuple
 from PIL import Image
 import numpy as np
 
 class BiomassMap:
 
-    def __init__(self, biomass_estimates: Dict, gps_location: Tuple[float, float]):
+    def __init__(self, biomass_estimates: np.ndarray, gps_location: Tuple[float, float]):
         self.biomass_estimates = biomass_estimates
         self.gps_location = gps_location
         self.map_image = Image.fromarray(np.zeros([1024, 1024, 3], dtype=np.uint8))
--- src/biomass_map_driver/scripts/tests/test_biomass_map.py
+++ src/biomass_map_driver/scripts/tests/test_biomass_map.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
 from biomass_map import BiomassMap
 import PIL
+import numpy as np
 
 class TestBiomassMap(TestCase):
 
     def test_run(self):
-        test_input_1 = {'AMAPA': 0.0, 'AMATU': 123.45, 'ECHCG': 234.56, 'PROLO': 0.0, 'ZEAMX': 0.0, 'GLXMA': 0.0}
+        test_input_1 = np.array([0.0, 123.45, 234.56, 0.0, 0.0, 0.0])
         test_input_2 = (3.456, 4.567)
         bio_map = BiomassMap(biomass_estimates=test_input_1, gps_location=test_input_2)
         map_results = bio_map.run()
