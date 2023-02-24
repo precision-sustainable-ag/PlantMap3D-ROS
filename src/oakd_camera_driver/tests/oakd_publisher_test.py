e7a615d9a68351b2c04a25e917e71a559a1f8372
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-02-23 21:29:17 -0500

    Testing message publish functionality [error active]

--- src/oakd_camera_driver/tests/oakd_publisher_test.py
+++ src/oakd_camera_driver/tests/oakd_publisher_test.py
@@ -3,8 +3,7 @@
 import unittest
 import rostest
 import os 
-import queue
-import threading
+import time
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 import roslib; roslib.load_manifest('oakd_camera_driver')
@@ -13,22 +12,19 @@ from rospy.numpy_msg import numpy_msg
 from oakd_camera_driver.msg import PM3DCameraData
 
 import numpy as np
-
+np.random.seed(45)
 from test_image_publisher import TestImagePublisher
 
 class TestImageSubscriber(unittest.TestCase):
 
-    def __init__(self,*args):
+    def setUp(self):
 
-        super(TestImagePublisher,self).__init__(*args)
         # initializing dummy camera data
         # rospy.init_node("oakd_camera_test")
-        image_publisher = TestImagePublisher()
-        self.queue = queue.Queue()
-        self.publisher_thread = threading.Thread(target=image_publisher.run())
-        self.subscriber_thread = threading.Thread(target=self.image_subscriber())
-        
-        self.array_data_flag, self.depth_data_flag, self.segmentation_label_arr_flag = image_publisher.return_current_camera_data()
+        rospy.init_node("camera_data_subscriber")
+        self.array_data = np.random.randint(0,30,(128,128,3),dtype=np.int64)
+        self.depth_data = np.random.randint(0,30,(128,128,3),dtype=np.int64)
+        self.segmentation_label_arr = np.random.randint(0,4,(128,128,3),dtype=np.int64)
         
         self.subscribed_rgb_data = None
         self.subscribed_depth_data = None 
@@ -40,18 +36,14 @@ class TestImageSubscriber(unittest.TestCase):
 
         self.subscribed_data = None
 
-        self.publisher_thread.start()
-        self.subscriber_thread.start()
-        self.publisher_thread.join()
-        self.subscriber_thread.join()
-       
-    
-    def image_subscriber(self):
+        self.success = False
+        self.sub = rospy.Subscriber("camera_1",numpy_msg(PM3DCameraData),callback=self.callback)
+        self.pub = rospy.Publisher("camera_1",numpy_msg(PM3DCameraData),queue_size=1)
+        self.msg = PM3DCameraData()
 
-        rospy.init_node("camera_data_subscriber")
-        rospy.Subscriber("camera_1",numpy_msg(PM3DCameraData),callback=self.callback)
-        rospy.spin()
-    
+    def tearDown(self):
+        self.sub.unregister()
+   
     def callback(self,data):
         rospy.loginfo("Appending Camera Data")
         self.subscribed_rgb_data = data.rgb_data
@@ -61,7 +53,10 @@ class TestImageSubscriber(unittest.TestCase):
         self.subscribed_depth_data_dims = data.depth_map_dims
         self.subscribed_segmentation_data_dims = data.segmentation_label_dims
 
+        
         self.subscribed_data = data
+        self.success = True
+        
 
 
     def test_check_camera_data_msg_type(self):
@@ -69,19 +64,37 @@ class TestImageSubscriber(unittest.TestCase):
         """
         Checking what msg type is being published
         """
-        
-        self.assertEqual(type(self.data),'PM3DCameraData')
+        self.msg.rgb_data = self.array_data.flatten().tolist()
+        self.msg.depth_map = self.depth_data.flatten().tolist()
+        self.msg.segmentation_labels = self.segmentation_label_arr.flatten().tolist()
 
+        self.msg.rgb_dims = self.array_data.shape
+        self.msg.depth_map_dims = self.depth_data.shape
+        self.msg.segmentation_label_dims = self.segmentation_label_arr.shape
 
-    def test_check_data_parameters(self):
+        
+        
+        timeout_t = time.time() + 10.0
+        while not rospy.is_shutdown and time.time() < timeout_t:
+            self.pub.publish(self.msg)
+            time.sleep(0.1)
+        
+        # self.pub.publish(self.msg)
 
-        """
-        Checking if the topic is publishing the correct data type and shape
-        """
+        rospy.sleep(2)
+        # test_data = self.subscribed_rgb_data.reshape((self.subscribed_rgb_data_dims))
+        rospy.loginfo(f"The variable is  {self.subscribed_rgb_data}")
+        # assert ((test_data == self.array_data).all()), "Arrays are not same"
+
+    # def test_check_data_parameters(self):
+
+    #     """
+    #     Checking if the topic is publishing the correct data type and shape
+    #     """
         
-        subscribed_rgb_data = self.subscribed_rgb_data.reshape((self.subscribed_rgb_data_dims))
-        assert ((subscribed_rgb_data == self.array_data_flag).all())
-        
+    #     subscribed_rgb_data = self.subscribed_rgb_data.reshape((self.subscribed_rgb_data_dims))
+    #     assert ((subscribed_rgb_data == self.array_data_flag).all())
+
 if __name__ == '__main__':
     
     rostest.rosrun('oakd_camera_driver','oakd_camera_test',TestImageSubscriber)
--- src/oakd_camera_driver/tests/test_image_publisher.py
+++ src/oakd_camera_driver/tests/test_image_publisher.py
@@ -11,14 +11,14 @@ from oakd_camera_driver.msg import PM3DCameraData
 from std_msgs.msg import Bool
 
 import numpy as np
-
+np.random.seed(45)
 class TestImagePublisher():
 
-    def __init__(self,node_name="camera_1",topic_name="camera_1"):
+    def __init__(self,topic_name="camera_1"):
         
-        rospy.init_node(node_name)
+        rospy.init_node("camera_data")
         self.image_name = np.random.randint(0,255,(1024,1024,3),dtype=np.int64)
-        self.node_name = node_name
+        self.node_name = "camera_data"
         self.topic_name = topic_name
         self.array_data = np.random.randint(0,30,(128,128,3),dtype=np.int64)
         self.depth_data = np.random.randint(0,30,(128,128,3),dtype=np.int64)
@@ -27,6 +27,7 @@ class TestImagePublisher():
         self.depth_data_flag = self.depth_data 
         self.segmentation_label_arr_flag = self.segmentation_label_arr 
         self.pub = rospy.Publisher(topic_name,numpy_msg(PM3DCameraData),queue_size=1)
+        self.run()
 
     def callback(self,data):
         
