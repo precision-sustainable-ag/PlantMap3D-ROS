8baa241a03ea7d1c0b090e2f8b02989ffbecf727
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-02-28 19:22:37 -0500

    Adding gps callback template for GPS heading

--- src/camera_trigger_driver/scripts/gps_heading_interpreter.py
+++ src/camera_trigger_driver/scripts/gps_heading_interpreter.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+
+import rospy 
+
+from std_msgs.msg import NavSatFix, NavSatStatus, TimeReference 
+from std_msgs import String
+import math 
+
+
+class GPSHeadingInterpreter():
+
+
+    def __init__(self):
+
+        rospy.init_node("gps_heading_interpreter")
+        self.messages = []
+        self.sub = rospy.Subscriber('/fix',NavSatFix,callback=self.callback)
+        self.pub = rospy.Publisher('fix/gps_heading',String,queue_size=1)
+
+
+    def callback(self,gps_fix_data):
+
+        self.messages.append(gps_fix_data)
+
+        if len(self.messages) == 5:
+
+            # do something
+            # debugging
+            print(self.messages) 
+            self.pub.publish(self.messages)
+            self.messages.clear()
+
+    def publish_gps_data(self):
+
+        pass 
+
