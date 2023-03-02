a08ae6909231436d353925fc43065a1e80b333bf
Plantmap3D <aaron.mathew0217@gmail.com>
2023-03-02 11:48:39 -0500

    Creating the template for gps heading file

--- src/gps_module_driver/scripts/gps_publisher.py
+++ src/gps_module_driver/scripts/gps_publisher.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python3
 
-import rospy 
+
 from std_msgs.msg import Bool
-import time
+import rospkg
+import rospy
+import rostest
+import yaml
+from sensor_msgs.msg import NavSatFix
 
-def get_gps_data(gps_string:str):
-
-    return gps_string 
-
-
-if __name__ == '__main__':
-
-    rospy.init_node("camera_trigger")
-
-    camera_trigger = True
-    # publishing data at 1Hz
-    rate = rospy.Rate(1)
-    pub = rospy.Publisher("camera_trigger",Bool,queue_size=1)
-    # gps_string = "$GNRMC,175800.00,A,3546.48455,N,07841.05018,W,0.212,,010223,,,A,V*04"
-
-    while not rospy.is_shutdown():
-        
-        rospy.loginfo(f"triggering camera : {camera_trigger}")
-        pub.publish(camera_trigger)
-        rate.sleep()
 
     
