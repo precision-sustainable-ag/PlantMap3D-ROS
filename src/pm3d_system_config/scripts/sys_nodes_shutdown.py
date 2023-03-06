2db5c2a70bb557cd3c8011d077812c982c88b0d3
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-03-05 20:35:34 -0500

    Adding comments

--- src/pm3d_system_config/scripts/sys_nodes_shutdown.py
+++ src/pm3d_system_config/scripts/sys_nodes_shutdown.py
@@ -10,26 +10,11 @@ if __name__ == '__main__':
 
     rospy.init_node("node_shutdown")
 
-    # log = Log()
-    # log.level = log.FATAL
-    # log.msg = 'shutdown'
-
-    # for i in range(5):
-    #     rospy.loginfo(log)
-    #     rospy.sleep(1)
-
     pub = rospy.Publisher('shutdown',String,queue_size=10)
 
     rospy.loginfo("Sleeping for 20 secs")
-    rospy.sleep(20)
+    rospy.sleep(15)
     pub.publish("shutdown")
     rospy.loginfo("Shutting down system...")
     
     rospy.sleep(1)
-    # rate = rospy.Rate(1)
-    # while True:
-
-    #     rospy.loginfo("Publishing data")
-    #     pub.publish("shutdown")
-        
-    #     rate.sleep()
