f11668dbf3a1225b5431b0a43983275a9a8015a7
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-03-09 15:06:02 -0500

    Optimizing shutdown by introducting an external node killer

--- src/pm3d_system_config/scripts/sys_nodes_shutdown.py
+++ src/pm3d_system_config/scripts/sys_nodes_shutdown.py
@@ -3,8 +3,17 @@
 import rospy 
 from std_msgs.msg import String 
 from rosgraph_msgs.msg import Log
+import rosnode
 
+def shutdown_all_nodes():
 
+    nodes_list = rosnode.get_node_names()
+
+    for node_name in nodes_list:
+
+        if node_name != rospy.get_name():
+            rospy.loginfo(f"Shutting down node : {node_name}")
+            rosnode.kill_nodes([node_name])
 
 if __name__ == '__main__':
 
@@ -12,9 +21,10 @@ if __name__ == '__main__':
 
     pub = rospy.Publisher('shutdown',String,queue_size=10)
 
-    rospy.loginfo("Sleeping for 20 secs")
-    rospy.sleep(50)
-    pub.publish("shutdown")
+    rospy.loginfo("Sleeping for 30 secs")
+    rospy.sleep(30)
+    #pub.publish("shutdown")
+    shutdown_all_nodes()
     rospy.loginfo("Shutting down system...")
     rospy.signal_shutdown("shutting down all nodes")
     rospy.sleep(1)
