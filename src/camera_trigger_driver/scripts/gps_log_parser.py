39053ec1a6733d867b6d959d6e8ee2f8a18a40e8
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-03-10 21:02:48 -0500

    GPS log parser

---
+++ src/camera_trigger_driver/scripts/gps_log_parser.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import NavSatFix, NavSatStatus
+import pynmea2
+
+
+if __name__ == '__main__':
+        
+    # Initialize ROS node
+    rospy.init_node('gps_node')
+
+    # Create publisher for NavSatFix messages
+    gps_pub = rospy.Publisher('gps', NavSatFix, queue_size=10)
+
+    # Set up file for reading GPS log data
+    log_file = open('gps_data.log', 'r')
+    
+    rate = rospy.Rate(1)
+    # Continuously read and publish GPS data
+    for line in log_file:
+        # Check if the line contains an RMC sentence
+        
+        if line.startswith('$GNRMC'):
+            try:
+                
+                # Parse the RMC sentence into a NMEA object
+                rmc = pynmea2.parse(line)
+                # Check if the GPS fix is valid
+                if rmc.status == 'A':
+                    # Create NavSatFix message and fill in data
+                    gps_msg = NavSatFix()
+                    gps_msg.latitude = rmc.latitude
+                    gps_msg.longitude = rmc.longitude
+                    gps_msg.altitude = 0.0
+                    gps_msg.status.status = NavSatStatus.STATUS_FIX
+                    gps_msg.status.service = NavSatStatus.SERVICE_GPS
+                    gps_msg.header.stamp = rospy.Time.now()
+                    # Publish the NavSatFix message
+                    # gps_pub.publish(gps_msg)
+
+                    rospy.loginfo(f"{gps_msg}")
+                    rate.sleep()
+            except pynmea2.ChecksumError:
+                continue
+
+    # Clean up
+    log_file.close()
--- src/camera_trigger_driver/scripts/test_gps_publisher.py
+++ src/camera_trigger_driver/scripts/test_gps_publisher.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 
-import unittest 
 import rospy 
+from std_msgs.msg import Bool
 
-class TestGPSPublisher(unittest.TestCase):
 
-    def __init__(self):
+def get_gps_data(gps_string:str):
 
-        pass
+    return gps_string 
+
+
+if __name__ == '__main__':
+
+    rospy.init_node("camera_trigger")
+
+    camera_trigger = True
+    rate = rospy.Rate(1)
+    pub = rospy.Publisher("camera_trigger",Bool,queue_size=1)
+    # gps_string = "$GNRMC,175800.00,A,3546.48455,N,07841.05018,W,0.212,,010223,,,A,V*04"
+
+    while not rospy.is_shutdown():
+        
+        rospy.loginfo(f"triggering camera : {camera_trigger}")
+        pub.publish(camera_trigger)
+        rate.sleep()
+
+    
