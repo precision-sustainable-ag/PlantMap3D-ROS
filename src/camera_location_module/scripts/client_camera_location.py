831ee8f730af81d4efe913795e53f3bb89ccf799
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-04-04 18:36:54 -0400

    Adding Camera Location service client and server code

--- src/camera_location_module/scripts/client_camera_location.py
+++ src/camera_location_module/scripts/client_camera_location.py
@@ -2,3 +2,22 @@
 
 import rospy
 from camera_location_module.srv import PM3DCameraLocation, PM3DCameraLocationRequest
+
+if __name__ == '__main__':
+
+    rospy.init_node("cam_location_client")
+    rospy.wait_for_service("camera_location_service")
+
+
+    try:
+        cam_location = rospy.ServiceProxy("camera_location_service",PM3DCameraLocation)
+        req = PM3DCameraLocationRequest()
+        req.gpscoords = [1.0,50.0]
+        req.gpsheading = 90.0
+        req.cameraid = 4
+        
+        resp = cam_location(req)
+        
+        rospy.loginfo(f"Received Camera 2 Location : {resp.newgpscoords}")
+    except rospy.ServiceException as e:
+        rospy.logerr("Camera Location Service Call Failed..")
--- src/camera_location_module/scripts/server_camera_location.py
+++ src/camera_location_module/scripts/server_camera_location.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy 
-from camera_location_module import PM3DCameraLocation
+from camera_location_module.srv import PM3DCameraLocation, PM3DCameraLocationResponse
 from camera_location import find_camera_gps_coordinates
+
 class CameraLocationInterpreter():
     """
     This class is a ROS server that takes in Camera Coords (lat and lon),
@@ -11,14 +12,20 @@ class CameraLocationInterpreter():
     def __init__(self):
 
         rospy.init_node("camera_location_interpreter")
+        self.camera_location_server()
 
     def camera_location_callback(self,data):
 
         rospy.loginfo(f"Printing GPS coords from camera location srv : {data.gpscoords}") 
-        return 0
+        camera_coords = find_camera_gps_coordinates(data.gpscoords,data.gpsheading,data.cameraid)
+        return PM3DCameraLocationResponse(camera_coords)
     
     def camera_location_server(self):
 
         rospy.loginfo(f"Camera Location service requested ")
         cam_location_service = rospy.Service('camera_location_service',PM3DCameraLocation,self.camera_location_callback)
         rospy.spin()
+
+if __name__ == "__main__":
+
+        camera_location_obj = CameraLocationInterpreter()
