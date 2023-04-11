6f4c91ab181aa5d79715b5e1aee89905081e1659
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-04-11 12:14:06 -0400

    Adding species_list file to config package

--- src/biomass_map_driver/scripts/biomass_map.py
+++ src/biomass_map_driver/scripts/biomass_map.py
@@ -7,11 +7,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 class BiomassMap:
 
-    def __init__(self, biomass_estimates: np.ndarray, gps_location: Tuple[float, float]):
+    def __init__(self, biomass_estimates: np.ndarray, gps_location: Tuple[float, float],species_list_path:str):
         self.biomass_estimates = biomass_estimates
         self.gps_location = gps_location
         # Opening the species list
-        with open("config/species_list.json", 'r') as species_file:
+        with open(species_list_path, 'r') as species_file:
             self.species_data = json.load(species_file)
         self.num_categories = len(self.species_data["species"])
         self.map_image = Image.fromarray(np.zeros([1024, 1024, 3], dtype=np.uint8))
--- src/biomass_map_driver/scripts/biomass_map_wrapper.py
+++ src/biomass_map_driver/scripts/biomass_map_wrapper.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 import roslib; roslib.load_manifest('oakd_camera_driver')
 import rospy
+import rospkg
 from rospy.numpy_msg import numpy_msg
 from oakd_camera_driver.msg import PM3DCameraData
 import numpy as np
 
 from biomass_map import BiomassMap
 
+rospy.init_node("biomass_map_driver")
+
+rospack = rospkg.RosPack()
+__path = rospack.get_path('configs') + '/config/species_list.json'
+
 def biomass_map_wrapper_callback(biomass_estimate_data):
     """
     This function outputs the biomass map generally in the form of PNG or JPEG
     """
-    biomass_map_object = BiomassMap(biomass_estimate_data.biomass_estimate,[1.0,1.0])
+    global __path
+    biomass_map_object = BiomassMap(biomass_estimate_data.biomass_estimate,[biomass_estimate_data.gps_data.latitude,biomass_estimate_data.gps_data.longitude],__path)
     biomass_map = biomass_map_object.run()
     print(f"Biomass map is : {biomass_map}")
 
 if __name__ == '__main__':
-
-    
-    rospy.init_node("biomass_map_driver")
 
     
     while not rospy.is_shutdown(): 
