66bf7ec242a34c470a835989c34f5bfb5d3c60c4
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-04-11 11:15:11 -0400

    Adding camera id to code

--- src/oakd_camera_driver/scripts/dummy_image_publisher_wrapper.py
+++ src/oakd_camera_driver/scripts/dummy_image_publisher_wrapper.py
@@ -10,6 +10,8 @@ from rospy.numpy_msg import numpy_msg
 from oakd_camera_driver.msg import PM3DCameraData
 import rospkg
 
+rospy.init_node("camera_wrapper")
+
 rospack = rospkg.RosPack()
 __path = rospack.get_path('configs') + '/config/image_config.yaml'
 
@@ -28,23 +30,19 @@ try :
 except FileNotFoundError as e:
     print(e)
 
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-"""
-    This is part of the test scripts
-"""
+# """
+#     This is part of the test scripts
+# """
 if __name__ == '__main__':
 
-    rospy.init_node("camera_wrapper")
-    # temporary camera id
-    cameraid = 0
     camera_data = []
-    for node_name in node_names :
     
+    for node_name in node_names :
+        cameraid = node_data[node_name]['camera_id']
         camera_data.append(["python3","image_publisher.py",node_name,str(cameraid)])
 
-    rospy.loginfo(camera_data)
     processes = []
     for cam_data in camera_data:
         
