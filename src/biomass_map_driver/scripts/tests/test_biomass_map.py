13510e1ceb2adc7b937cd2001f070767f49ff50b
Aaron Mathew <aaron.mathew0217@gmail.com>
2023-05-01 13:25:43 -0400

    Adding biomass csv initial test template and file name changes

--- src/biomass_map_driver/scripts/tests/test_biomass_map.py
+++ src/biomass_map_driver/scripts/tests/test_biomass_map.py
@@ -1,15 +1,59 @@
 #!/usr/bin/env python3
 from unittest import TestCase
-from biomass_map import BiomassMap
-import PIL
-import numpy as np
+import rostest 
 
+import roslib; roslib.load_manifest('biomass_map_driver')
+import rospy 
+import rospkg
+from collections import Counter
+from biomass_map_driver.srv import PM3DBiomassDataSaver, PM3DBiomassDataSaverRequest
+import numpy as np
+np.random.seed(45)
+import time
+import os, sys 
+import datetime, pytz
 class TestBiomassMap(TestCase):
 
-    def test_run(self):
-        test_input_1 = np.array([0.0, 123.45, 234.56, 0.0, 0.0, 0.0])
-        test_input_2 = (3.456, 4.567)
-        bio_map = BiomassMap(biomass_estimates=test_input_1, gps_location=test_input_2)
-        map_results = bio_map.run()
-        print(type(map_results))
-        self.assertEqual(type(map_results), PIL.Image.Image)
+    def setup(self):
+
+        rospy.init_node('biomass_saver_test')
+
+    def test_append_csv_file(self):
+
+        rospy.wait_for_service('biomass_data_saver')
+        rospack_testset = rospkg.RosPack()
+        current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+        summary_filename = "test_biomass_data_" + str(current_time.year) + "-" + str(current_time.month) + "-" + str(current_time.day) + ".csv"
+        __biomass_summary_path= rospack_testset.get_path('data_saver_driver') + '/biomass_estimation/biomass_save_summary/' + str(summary_filename)
+        
+        try :
+
+            data_saver = rospy.ServiceProxy('biomass_data_saver',PM3DBiomassDataSaver)
+
+            req = PM3DBiomassDataSaverRequest()
+            req.summary_path_and_name = __biomass_summary_path
+            req.image_name = "test.jpg"
+            req.latitude = -39.1
+            req.longitude = 71.2
+            req.grass_pixels = 1
+            req.grass_biomass = 2
+            req.clover_pixels = 3
+            req.clover_biomass = 4
+            req.brassica_pixels = 5
+            req.brassica_biomass = 6
+            req.weed_pixels = 7
+            req.weed_biomass = 8
+            req.total_vegetation_pixels = 9
+            req.total_biomass = 10
+
+            resp = data_saver(req)
+
+            rospy.loginfo(f"Saving {req} data to biomass summary...")
+            rospy.loginfo(f"Response from server : {resp}")
+
+        except rospy.ServiceException as e :
+            rospy.logerr("Camera location service call failed..")
+
+if __name__ == '__main__':
+    
+    rostest.rosrun('biomass_map_driver','test_biomass_map.py',TestBiomassMap)
